e111827937fa1fa20a75e5712a9f7c63438f597d
Amit <you@example.com>
2021-01-04 13:25:11 +0100

    Update buildspec

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,7 +3,6 @@ const tasks = t => t.join(' && ')
 module.exports = {
   hooks: {
     'pre-commit': tasks([
-      'terraform fmt -recursive -check ./terraform/',
       "npm run lint",
     ])
   }
